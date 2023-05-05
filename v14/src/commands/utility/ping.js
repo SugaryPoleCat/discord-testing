ee8f4761ecf37b00dc47a24f5749a316b795d140
Sugar <sugarypolecat@gmail.com>
2023-05-05 22:54:02 +0200

    So that was on me I was dumb It works now I just put GET instead of SET when registering commands in the app on start.,

--- v14/src/commands/utility/ping.js
+++ v14/src/commands/utility/ping.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder().setName("ping").setDescription("Replies wiht pong!"),
-	async execute(interaction) { await interaction.reply("Pong!"); }
+	async execute(interaction) {
+		console.log("interaction in command: ", interaction);
+		await interaction.reply("Pong!");
+	}
 };
--- v14/src/index.js
+++ v14/src/index.js
@@ -5,15 +5,15 @@ const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 // const TOKEN = String(process.env.TOKEN);
 const { TOKEN } = require("../config.json");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter((f) => f.endsWith(".js"));
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
-	if (event.once) { client.once(event.name, (...args) => { event.execute(...args); }); }
-	else { client.on(event.name, (...args) => { event.execute(...args); }); }
+	if (event.once) { client.once(event.name, async (...args) => { await event.execute(...args); }); }
+	else { client.on(event.name, async (...args) => { await event.execute(...args); }); }
 }
-client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
@@ -22,7 +22,8 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		if ("data" in command && "execute" in command) { console.log("command registered"); client.commands.get(command.data.name, command); }
+		console.log(command);
+		if ("data" in command && "execute" in command) { client.commands.set(command.data.name, command); }
 		else { console.error(`[WARNING] at ${new Date().toUTCString()}\nThe command at ${filePath} is missing a required "data" or "execute" property`); }
 	}
 }
