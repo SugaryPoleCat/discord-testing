8da97726b8f42b80901f0c3909d30262da347a5c
Sugar <sugarypolecat@gmail.com>
2023-05-08 19:10:51 +0200

    doen with emoji

--- v14-ts/src/commands/shards/emoji.ts
+++ v14-ts/src/commands/shards/emoji.ts
@@ -1,7 +1,11 @@
 import command from "../../utilities/command";
 import path from "path";
 function findEmoji(c, { nameOrId }) {
-	return c.emojis.cache.get[nameOrId] || c.emojis.cache.find((e) => e.name.toLowerCase() === nameOrId.toLowerCase());
+	const emoji = c.emojis.cache.get[nameOrId] || c.emojis.cache.find((e) => e.name.toLowerCase() === nameOrId.toLowerCase());
+	if (!emoji) { return null; }
+	// you can delete the emioji too.
+	// emoji.delete();
+	return emoji;
 }
 module.exports = {
 	data: command({ name: path.basename(__filename), description: "Using functions extended" })
@@ -9,6 +13,10 @@ module.exports = {
 	async fox(interaction, client) {
 		const emoji = interaction.options.getString("poop");
 		return client.shard.broadcastEval(findEmoji, { context: { nameOrId: emoji } })
-			.then(console.log);
+			.then((emojiArray) => {
+				const foundEmoji = emojiArray.find((emoji) => emoji);
+				if (!foundEmoji) { return interaction.reply("couldnt find your emoji nerd"); }
+				return interaction.reply(`found your emoji nerd: ${foundEmoji.animated ? `<${foundEmoji.identifier}>` : `<:${foundEmoji.identifier}> emoji!`}!`);
+			});
 	}
 };
